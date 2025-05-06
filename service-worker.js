35e0c4c05417af953a4819428095c611a1befdbb
Re-EL123 <147254957+Re-EL123@users.noreply.github.com>
2025-05-06 07:27:30 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
 
+const CACHE_NAME = 're-player-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+  );
+});
